eccb706605fd51694417d4202522a4149a01bbdd
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-07-31 17:30:55 -0300

    Remove Swagger

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,7 +3,6 @@ import "dotenv/config";
 import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
-import swaggerUi from "swagger-ui-express";
 
 import upload from "@config/upload";
 import * as Sentry from "@sentry/node";
@@ -12,8 +11,6 @@ import { AppError } from "@shared/errors/AppError";
 import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
 import { router } from "@shared/infra/http/routes";
 import createConnection from "@shared/infra/typeorm";
-
-import swaggerFile from "../../../swagger.json";
 
 import "@shared/container";
 
@@ -37,8 +34,6 @@ if (process.env.SENTRY_DSN.length > 50) {
 }
 
 app.use(express.json());
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
